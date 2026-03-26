31e289bbb2ad7a08b3d2b52b057a2e058a5e5179
Abdel Fane <abdelsyfane@gmail.com>
2026-03-26 09:27:10 -0600

    chore: exclude daemon node_modules and dist-test from git

--- packages/nanomind-daemon/dist-test/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { NanoMindDaemon, DEFAULT_CONFIG } from './server.js';
-export type { DaemonConfig, InferRequest, InferResponse } from './server.js';
-export { BaseFabricAdapter } from './fabric-adapter.js';
-export type { FabricAdapter, RiskSummary, InferContext } from './fabric-adapter.js';
--- packages/nanomind-daemon/dist-test/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseFabricAdapter = exports.DEFAULT_CONFIG = exports.NanoMindDaemon = void 0;
-var server_js_1 = require("./server.js");
-Object.defineProperty(exports, "NanoMindDaemon", { enumerable: true, get: function () { return server_js_1.NanoMindDaemon; } });
-Object.defineProperty(exports, "DEFAULT_CONFIG", { enumerable: true, get: function () { return server_js_1.DEFAULT_CONFIG; } });
-var fabric_adapter_js_1 = require("./fabric-adapter.js");
-Object.defineProperty(exports, "BaseFabricAdapter", { enumerable: true, get: function () { return fabric_adapter_js_1.BaseFabricAdapter; } });
